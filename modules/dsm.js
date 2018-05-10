f1810d3c63e5821c1a2199b872b73e2bc107a445
Rex Tseng <rexx0520@gmail.com>
2018-05-11 00:25:50 +0800

    dsm.js init

---
+++ modules/dsm.js
@@ -0,0 +1,51 @@
+const Syno = require('syno');
+
+class Dsm {
+    constructor(protocol, host, port, account, passwd, apiVersion){
+        this._syno = new Syno({
+            // Requests protocol : 'http' or 'https' (default: http)
+            protocol: protocol,
+            // DSM host : ip, domain name (default: localhost)
+            host: host,
+            // DSM port : port number (default: 5000)
+            port: port,
+            // DSM User account (required)
+            account: account,
+            // DSM User password (required)
+            passwd: passwd,
+            // DSM API version (optional, default: 6.0.2)
+            apiVersion: apiVersion
+        });
+    }
+    tasks(){
+        let tasks = {}
+        this._syno.dl.listTasks(resp => resp.tasks.forEach(task => tasks[task.id] = task))
+        return tasks
+        
+        /*
+        {
+            "id": {
+                "id": "dbid_141",
+                "size": 38981861376,
+                "status": "paused", // paused, error, finished, downloading, pending
+                "title": "TASKNAME",
+                "type": "bt", // bt, https, http
+                "username": "ESC"
+            }
+        }
+        */
+    }
+
+    getInfo(){
+        return this._syno.dl.getInfo()
+        
+        /*
+            {
+                "is_manager":true,
+                "version":2269,
+                "version_string":"3.2-2269"
+            }
+        */
+    }
+}
+
